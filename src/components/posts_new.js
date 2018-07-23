ac0d66db89839ba52c6cad3d29b4cf0fb9314075
vikalp kumar <vikkumar1@WKMIN7374032.global.publicisgroupe.net>
2018-07-23 17:00:29 +0530

    react redux routs and forms

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        Form Here;
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
